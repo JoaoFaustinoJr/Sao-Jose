be40d2372acb309eb6320a676401d970702d66ca
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-18 10:42:11 -0300

    v4.2: refresh complete chapter vellum update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sao-jose-v41';
+const CACHE='sao-jose-v42';
 const ASSETS=['./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
