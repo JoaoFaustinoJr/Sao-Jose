9a1c889ba9e6bd1a4e4c544e513c293bf3c19f9d
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-18 10:47:06 -0300

    v4.4: refresh isolated vellum fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sao-jose-v43';
+const CACHE='sao-jose-v44';
 const ASSETS=['./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
