0b4659510e6c5f15b5df29a66e6a286540179309
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-23 08:30:29 -0300

    Renova cache PWA 11.55

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sao-jose-v1154';
-const SHELL=['./index.html','./styles.css?v=254','./app.js?v=254','./manifest.webmanifest?v=254'];
+const CACHE='sao-jose-v1155';
+const SHELL=['./index.html','./styles.css?v=255','./app.js?v=255','./manifest.webmanifest?v=255'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
